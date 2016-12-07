61a25ae608c0380c5e64ac519ce9176df44749bd
pmontu <pmontu@gmail.com>
2016-12-07 12:14:43 +0530

    form csrf post

---
+++ trial/views.py
@@ -0,0 +1,13 @@
+from django.http  import HttpResponse
+from django.middleware import csrf
+
+def hello(request):
+	csrf_token = csrf.get_token(request)
+	return HttpResponse("Hello<br>My Name is Manoj Kumar<br><form method='post' action='message'>{0}<input name='message' type=text value='hi'></input><input type='submit'></input></form>".format('<input type="hidden" name="csrfmiddlewaretoken" value="%s">' % csrf_token))
+
+def message(request):
+	# import ipdb; ipdb.set_trace()
+	message = "empty"
+	if request.method == 'POST' and "message" in request.POST:
+		message = request.POST.get("message")
+	return HttpResponse(message)
